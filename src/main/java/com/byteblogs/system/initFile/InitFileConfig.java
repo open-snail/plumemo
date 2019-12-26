ba3197821754b4fa02bb27f48e5ca9040442e077
青涩知夏 <1748881069@qq.com>
2019-12-26 19:10:57 +0800

    青涩知夏-》代码优化

--- src/main/java/com/byteblogs/system/initFile/InitFileConfig.java
+++ src/main/java/com/byteblogs/system/initFile/InitFileConfig.java
@@ -10,13 +10,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Objects;
 
 @Component
 @Slf4j
-public class InitFileConfig implements ApplicationListener<ApplicationContextEvent>, Ordered {
+public class InitFileConfig implements ApplicationListener<ApplicationContextEvent> {
 
     private String FILE_PATH=System.getProperty("java.home").replaceFirst("jre","bin")+File.separator;
-    private String RESOURCES_PATH=InitFileConfig.class.getClassLoader().getResource("dll").getPath()+File.separator;
+    private String RESOURCES_PATH= Objects.requireNonNull(InitFileConfig.class.getClassLoader().getResource("dll")).getPath()+File.separator;
 
     public String getFileName(){
         if (isWindows()){
@@ -25,26 +26,22 @@ public class InitFileConfig implements ApplicationListener<ApplicationContextEve
             return "libsigar-amd64-linux.so";
         }
     }
-
     public static boolean isWindows() {
         return System.getProperty("os.name").toLowerCase().contains("windows");
-    }
-    public static boolean isLinux() {
-        return System.getProperty("os.name").toLowerCase().contains("linux");
     }
 
     public  void initFile(){
         FileInputStream fis = null;
         FileOutputStream fos = null;
        try{
-           fis = new FileInputStream(new File(RESOURCES_PATH+getFileName()));//创建输入流对象
            File file=new File(FILE_PATH+getFileName());
            if (!file.exists()){
+               fis = new FileInputStream(new File(RESOURCES_PATH+getFileName()));//创建输入流对象
                fos = new FileOutputStream(FILE_PATH+getFileName()); //创建输出流对象
-               byte datas[] = new byte[1024];//创建搬运工具
+               byte data[] = new byte[1024];//创建搬运工具
                int len = 0;//创建长度
-               while((len = fis.read(datas))!=-1){
-                   fos.write(datas,0,len);
+               while((len = fis.read(data))!=-1){
+                   fos.write(data,0,len);
                }
            }
        } catch (Exception e){e.printStackTrace();}
@@ -57,10 +54,5 @@ public class InitFileConfig implements ApplicationListener<ApplicationContextEve
     @Override
     public void onApplicationEvent(ApplicationContextEvent event) {
         initFile();
-    }
-
-    @Override
-    public int getOrder() {
-        return 2;
     }
 }
