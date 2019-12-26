ac55840561fd2b917f604e960fb8f32da367dbe8
huanghuijie <1748881069@qq.com>
2019-12-26 09:01:43 +0800

    青涩知夏-》开启异步支持

--- src/main/java/com/byteblogs/HelloBlogApplication.java
+++ src/main/java/com/byteblogs/HelloBlogApplication.java
@@ -4,6 +4,7 @@ import com.byteblogs.system.init.ConfigApplicationContextInitializer;
 import com.byteblogs.system.listener.LoadConfigListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class HelloBlogApplication {
 
     public static void main(String[] args) {
