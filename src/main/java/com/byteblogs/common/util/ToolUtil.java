3b88d9321d000732bc1daec2332c22dca4d26bf8
huanghuijie <1748881069@qq.com>
2019-12-31 15:01:14 +0800

    青涩知夏：优化工具方法

--- src/main/java/com/byteblogs/common/util/ToolUtil.java
+++ src/main/java/com/byteblogs/common/util/ToolUtil.java
@@ -2,9 +2,13 @@ package com.byteblogs.common.util;
 
 public class ToolUtil {
     public static Integer getInteger(Object obj){
-        if (obj==null){
+        try{
+            if (obj==null){
+                return 0;
+            }
+            return Integer.parseInt(obj.toString());
+        }catch (Exception e){
             return 0;
         }
-        return Integer.parseInt(obj.toString());
     }
 }
