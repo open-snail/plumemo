726061450876e6e9c9fcf297902661646f23dda0
huanghuijie <1748881069@qq.com>
2020-01-21 10:43:29 +0800

    青涩知夏：删除无用项

--- src/main/java/com/byteblogs/common/config/WebConfigurer.java
+++ src/main/java/com/byteblogs/common/config/WebConfigurer.java
@@ -17,12 +17,6 @@ import java.util.List;
 class WebConfigurer implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        final ConfigDao configDao = BeanTool.getBean(ConfigDao.class);
-        final List<Config> configList = configDao.selectList(null);
-        configList.forEach(config -> {
-            ConfigCache.putConfig(config.getConfigKey(), config.getConfigValue());
-        });
-
         System.out.println("动态注入成功："+ConfigCache.getConfig(Constants.DEFAULT_PATH));
         registry.addResourceHandler("/files/**").addResourceLocations("file:///"+ ConfigCache.getConfig(Constants.DEFAULT_PATH));
     }
