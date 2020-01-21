467d4620711f73c236300661d8fb151c0b40164f
byteblogs168 <byteblogs168@aliyun.com>
2020-01-21 23:18:01 +0800

    添加配置

--- src/main/java/com/byteblogs/system/init/InitSystemConfig.java
+++ src/main/java/com/byteblogs/system/init/InitSystemConfig.java
@@ -1,9 +1,15 @@
 package com.byteblogs.system.init;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.byteblogs.common.cache.ConfigCache;
 import com.byteblogs.common.config.WebConfigurer;
+import com.byteblogs.common.context.BeanTool;
+import com.byteblogs.helloblog.auth.dao.AuthUserDao;
+import com.byteblogs.helloblog.auth.domain.po.AuthUser;
+import com.byteblogs.helloblog.bean.SystemPropertyBean;
 import com.byteblogs.helloblog.config.dao.ConfigDao;
 import com.byteblogs.helloblog.config.domain.po.Config;
+import com.byteblogs.system.enums.RoleEnum;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -14,6 +20,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -31,6 +38,9 @@ public class InitSystemConfig implements ImportBeanDefinitionRegistrar {
     @Autowired
     private ConfigDao configDao;
 
+    @Autowired
+    private AuthUserDao authUserDao;
+
     @PostConstruct
     public void init() {
         final List<Config> configList = configDao.selectList(null);
@@ -38,6 +48,13 @@ public class InitSystemConfig implements ImportBeanDefinitionRegistrar {
             log.debug("config_key: {}, config_vlaue: {}", config.getConfigKey(), config.getConfigValue());
             ConfigCache.putConfig(config.getConfigKey(), config.getConfigValue());
         });
+
+        List<AuthUser> authUsers = authUserDao.selectList(new LambdaQueryWrapper<AuthUser>().eq(AuthUser::getRoleId, RoleEnum.ADMIN.getRoleId()));
+        if (!CollectionUtils.isEmpty(authUsers)) {
+            SystemPropertyBean systemPropertyBean = BeanTool.getBean(SystemPropertyBean.class);
+            systemPropertyBean.setSocialId(authUsers.get(0).getSocialId());
+        }
+
     }
 
     /**
