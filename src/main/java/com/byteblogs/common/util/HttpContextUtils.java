f247b46e81926e8c98e15afc63fe42fccfb1144a
huanghuijie <1748881069@qq.com>
2020-02-26 09:10:52 +0800

    青涩知夏：修复获取访问者信息失败导致的接口无法访问

--- src/main/java/com/byteblogs/common/util/HttpContextUtils.java
+++ src/main/java/com/byteblogs/common/util/HttpContextUtils.java
@@ -53,30 +53,44 @@ public class HttpContextUtils {
 	 * 获取发起请求的浏览器名称
 	 */
 	public static String getBrowserName(HttpServletRequest request) {
-		String header = request.getHeader("User-Agent");
-		UserAgent userAgent = UserAgent.parseUserAgentString(header);
-		Browser browser = userAgent.getBrowser();
-		return browser.getName();
+		try{
+			String header = request.getHeader("User-Agent");
+			UserAgent userAgent = UserAgent.parseUserAgentString(header);
+			Browser browser = userAgent.getBrowser();
+			return browser.getName();
+		}catch (Exception e){
+			return "not found name";
+		}
 	}
 
 	/**
 	 * 获取发起请求的浏览器版本号
 	 */
 	public static String getBrowserVersion(HttpServletRequest request) {
-		String header = request.getHeader("User-Agent");
-		UserAgent userAgent = UserAgent.parseUserAgentString(header);
-		Browser browser = userAgent.getBrowser();// 获取浏览器信息
-		Version version = browser.getVersion(header);// 获取浏览器版本号
-		return version.getVersion();
+		try{
+			String header = request.getHeader("User-Agent");
+			UserAgent userAgent = UserAgent.parseUserAgentString(header);
+			Browser browser = userAgent.getBrowser();// 获取浏览器信息
+			Version version = browser.getVersion(header);// 获取浏览器版本号
+			return version.getVersion();
+		}catch (Exception e){
+			return "not found version";
+		}
+
 	}
 
 	/**
 	 * 获取发起请求的操作系统名称
 	 */
 	public static String getOsName(HttpServletRequest request) {
-		String header = request.getHeader("User-Agent");
-		UserAgent userAgent = UserAgent.parseUserAgentString(header);
-		OperatingSystem operatingSystem = userAgent.getOperatingSystem();
-		return operatingSystem.getName();
+		try{
+			String header = request.getHeader("User-Agent");
+			UserAgent userAgent = UserAgent.parseUserAgentString(header);
+			OperatingSystem operatingSystem = userAgent.getOperatingSystem();
+			return operatingSystem.getName();
+		}catch (Exception e){
+			return "not found osName";
+		}
+
 	}
 }
